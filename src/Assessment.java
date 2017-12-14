7f6ea8bad1274e56354e5d0cca9e2883aa7eb63e
AlishiaWilcher <stown400@gmail.com>
2017-12-14 15:17:09 -0600

    completed average problem

--- src/Assessment.java
+++ src/Assessment.java
@@ -2,26 +2,38 @@ import java.util.Scanner;
 
 public class Assessment {
 
-        public static void main(String [] args)
-        {
+        public static void main(String [] args) {
+
+            System.out.println("Enter length of the array?");
 
             Scanner in = new Scanner(System.in);
+            int length = in.nextInt();
+            int[] input = new int[length];
 
-            double firstNumber, secondNumber;
-            double sum;
+            System.out.println("Please enter numbers");
+            for (int i = 0; i < length; i++) {
+                input[i] = in.nextInt();
+            }
 
-            System.out.print("Please enter first number: ");
-            firstNumber = in.nextInt();
+            double average = average(input);
 
-            System.out.println("Enter your second number: ");
-            secondNumber = in.nextInt();
+            System.out.println("Average of all number in array is " + average);
+        }
 
-            sum = firstNumber + secondNumber;
-            System.out.println("Sum of the two numbers are: " + sum);
+
+           public static float average(int[] input){
+               float sum = 0f;
+               for(int number : input) {
+                   sum = sum + number;
+               }
+               return sum / input.length;
+            }
+
+
 
         }
 
-    }
+
 
 
 
